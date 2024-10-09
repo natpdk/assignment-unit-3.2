c1d3cdecfe803bd2212fb4ea76a015b299629231
Nat Koch <nat.pd.koch@gmail.com>
2024-10-09 09:44:07 -0700

    Answered question 2

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -50,13 +50,16 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for( starName of stars ){
+  console.log( 'for of loop', starName );
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
