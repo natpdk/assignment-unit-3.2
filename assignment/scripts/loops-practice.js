a0b9234a6e8f88420e6c340a6eebed9e550c5a82
Nat Koch <nat.pd.koch@gmail.com>
2024-10-09 10:14:19 -0700

    Answered question 3.

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -59,10 +59,27 @@ console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+let i = 0;
 
+while (i < stars.length) {
+  console.log(stars[i]);
+  i++;
+}
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
+let index = 0;
+const max = 5
+while( index <= max ){
+  console.log( 'in while loop. index:', index );
+  index ++;
+}
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+let count = 10;
+
+while (count >= 5) {
+  console.log(count);
+  count--;
+}
